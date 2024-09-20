ccbc9f99fc546d276afab4731e56fd46d977819f
MartSasha <martsasha1998@yandex.ru>
2024-09-20 13:35:02 +0300

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,5 @@
+sum_homework=12
+sum_hours_spent=1.5
+name_course='Pyton'
+time_of_one_lesson=sum_hours_spent/sum_homework
+print('Курс:',name_course,',','всего задач:',sum_homework, ',','затрачено часов:', sum_hours_spent,',','среднее время выполнения:',time_of_one_lesson)
