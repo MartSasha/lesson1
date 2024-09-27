2ffd6116f427e9c147966e67697e829f8b08cbd3
MartSasha <martsasha1998@yandex.ru>
2024-09-27 11:46:56 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,5 @@
+immutable_var=(1, True, "apple", [5,6])
+print(immutable_var)
+immutable_var[0]=2 #элементы кортежа нельзя изменять, если только мы не изменяем список внутри него
+mutable_list=["name", "hello", "good"]
+print(mutable_list)
